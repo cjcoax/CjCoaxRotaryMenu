80de4a660639efe6b4dda7e371eb401b406b0c14
Amir Rezvani <amirrezvani@amirs-mbp>
2017-07-08 13:11:01 -0700

    Initial Commit

---
+++ CjCoaxRotatyMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CjCoaxRotatyMenu
+//
+//  Created by Amir Rezvani on 7/8/17.
+//  Copyright © 2017 Amir Rezvani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
